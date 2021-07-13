0e75334ccd52ce5432afbc61cb3a5a19d5ed76da
Nick Cao <nickcao@nichi.co>
2021-07-13 09:17:07 +0800

    added support for calibration

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![feature(duration_constants)]
 use clap::Clap;
+use dialoguer::Confirm;
 use nizctl::{config, keyboard};
 use std::io::Read;
 
@@ -14,6 +15,9 @@ struct Nizctl {
 enum SubCommand {
     Pull(Pull),
     Push(Push),
+    Lock(Lock),
+    Unlock(Unlock),
+    Calib(Calib),
 }
 
 #[derive(Clap, Debug)]
@@ -24,6 +28,15 @@ struct Pull {
 
 #[derive(Clap, Debug)]
 struct Push {}
+
+#[derive(Clap, Debug)]
+struct Lock {}
+
+#[derive(Clap, Debug)]
+struct Unlock {}
+
+#[derive(Clap, Debug)]
+struct Calib {}
 
 fn main() {
     let opts: Nizctl = Nizctl::parse();
@@ -46,6 +59,32 @@ fn main() {
                     config::Keymap::decode(&buffer).unwrap().layers,
                 ))
                 .unwrap()
+        }
+        SubCommand::Lock(_) => {
+            if Confirm::new()
+                .with_prompt("do you really want to lock your keyboard, you will need another keyboard to unlock")
+                .interact().unwrap()
+            {
+                keyboard::Keyboard::open().unwrap().keylock().unwrap();
+            }
+        }
+        SubCommand::Unlock(_) => {
+            keyboard::Keyboard::open().unwrap().keyunlock().unwrap();
+        }
+        SubCommand::Calib(_) => {
+            if !Confirm::new()
+                .with_prompt("you will need another keyboard to continue with the calibration process, as you keyboard will be locked, before continuing, make sure that all the keys are released")
+               .interact().unwrap()
+            {
+                return;
+            }
+            let kbd = keyboard::Keyboard::open().unwrap();
+            kbd.keylock().unwrap();
+            kbd.calib().unwrap();
+            while Confirm::new().with_prompt("hold the key you want to calibrate firmly, then press y from another keyboard, press n when you are done").interact().unwrap() {
+                kbd.calib_press().unwrap();
+            }
+            kbd.keyunlock().unwrap();
         }
     }
 }
